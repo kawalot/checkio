6c2268c5b7e21191deeae62d0db3e0de2906a00e
AlexandrPescherov <kawalot@gmail.com>
2017-04-03 22:32:34 +0300

    Task Binary count done.

---
+++ Binary-count.py
@@ -0,0 +1,16 @@
+"""
+Input: A number as a positive integer.
+Output: The quantity of unities in the binary form as an integer. 
+"""
+
+
+def checkio(number):
+    return bin(number).count('1')
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(4) == 1
+    assert checkio(15) == 4
+    assert checkio(1) == 1
+    assert checkio(1022) == 9
