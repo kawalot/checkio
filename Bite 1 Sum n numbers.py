bf275e6a78d7a44c649456e973aaf823488b7858
AleksandrPeshcherov <kawalot@gmail.com>
2018-01-22 23:10:50 +0200

    Sum n numbers reference

--- Bite 1 Sum n numbers.py
+++ Bite 1 Sum n numbers.py
@@ -1,8 +1,13 @@
+# def sum_numbers(numbers=None):
+#     sum = 0
+#     if numbers or numbers==[]:
+#         for i in numbers:
+#             sum += i
+#     else:
+#         return 5050
+#     return sum
+
 def sum_numbers(numbers=None):
-    sum = 0
-    if numbers or numbers==[]:
-        for i in numbers:
-            sum += i
-    else:
-        return 5050
-    return sum
+    if numbers is None:
+        numbers = range(1, 101)
+    return sum(numbers)
